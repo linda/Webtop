c2fd604138f0ea4ee105cbd74a4d2f1535079dca
linda23 <if13b076@technikum-wien.at>
2014-09-03 16:59:48 +0200

    files can be renamed

--- rename.php
+++ rename.php
@@ -9,19 +9,24 @@
 	// Rename the image sent with GET
 	// ============================
 		$imagetorename = $_GET['rename'];
-		$imageSeparated = explode(".", $imagetorename);
-		$imageExtension = $imageSeparated[1];
-		$imageName = $imageSeparated[0];
+		// $imageSeparated = explode(".", $imagetorename);
+		// $imageExtension = $imageSeparated[1];
+		// $imageName = $imageSeparated[0];
 		echo "Page for renaming this image: " . $imagetorename . "<br>";
 		
 	if(isset($_POST['newname'])){
 		$newName = $_POST['newname'];
-		echo "New name for this pic: " . $newName;
+		if (rename('images/'.$imagetorename, 'images/'.$newName))
+			echo "Image successfully renamed to " . $newName;
+		else echo "Could not rename";
 	}
 ?>
 <html>
 <body>
 		<form action="rename.php<?php echo'?rename='.$imagetorename; ?>" method="POST">
-			New name: <input type="text" name="newname"> . <?php echo $imageExtension;?>
-			<p><input type="submit" value="newname">
+			New name: <input type="text" name="newname">
+			<p><input type="submit" value="rename">
 	</form>
+	<a href="photoapp.php">Back to overview</a>
+</body>
+</html>	
